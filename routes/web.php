ad5ca489f7bec8b86be41ff844972ac3f27c54b1
root <root@vmi229649.contaboserver.net>
2019-01-23 07:35:54 +0100

    f1

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (){
     return view('welcome');
+});
+
+Route::get('/admin', function () {
+    return view('welcomeadmin');
 });
 
 Auth::routes();
@@ -21,4 +25,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/product', function (){
+    return view('single-product-details');
+} );

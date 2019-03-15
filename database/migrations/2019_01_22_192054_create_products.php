7345324ff3db9cc8a5ee69dfc902170ec2086a76
Your Name <you@example.com>
2019-03-16 04:22:39 +0700

    1

--- database/migrations/2019_01_22_192054_create_products.php
+++ database/migrations/2019_01_22_192054_create_products.php
@@ -14,7 +14,15 @@ class CreateProducts extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->index();
+            $table->string('name')->nullable();
+            $table->string('title')->nullable();
+            $table->string('url_image')->nullable();
+            $table->string('detail_long')->nullable();
+            $table->string('detail_short')->nullable();
+            $table->string('price')->nullable();
+            $table->string('type')->nullable();
+            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2019_03_15_174046_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('email')->nullable();
+            $table->string('full_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->string('zip')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
